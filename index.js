a58fc19a9d75cc392df4b9dde8d3972d1ee8f9f6
jaimeh760 <jaimeh760@gmail.com>
2016-11-02 15:50:31 +0000

    Third files from Nitrous

---
+++ index.js
@@ -0,0 +1,16 @@
+var greeting = "Hello, everybody!";
+
+var specialGuest = "Neil deGrasse Tyson";
+
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
+
+var topic = "space";
+
+var conversation = `Let\'s talk about ${topic}.`;
+
+var writeGreetingToDocument = 'document.getElementById(\'greeting\').innerHTML = greeting;';
+
+var customCelebration = 'Kaboomie! Hachewie, yeaaaaaaaah. Shwing.';
+
+var consoleCelebration = 'console.log(customCelebration);';
+
